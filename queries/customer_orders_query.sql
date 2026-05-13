57eec7348a945b8efca3cbd0412427f8dc02475d
Anil Sapkota <45574896+anilsapkota@users.noreply.github.com>
2026-05-13 20:54:47 +0000

    created a new csv based on the sql

--- queries/customer_orders_query.sql
+++ queries/customer_orders_query.sql
@@ -1,4 +1,10 @@
-select *,
-       row_number() over(partition by order_number, order_total,order_date order by order_date) as row_num
+select customer_orders_table.*
+       , customer_shipping_table.CustState 
+       --,row_number() over(partition by order_number, order_total,order_date order by order_date) as row_num
 from customer_orders_table
-where order_number is null 
+inner join customer_shipping_table 
+on customer_orders_table.order_number = customer_shipping_table.OrderNum 
+
+
+select *
+from customer_shipping_table
